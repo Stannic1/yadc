41507e00449a5861c1aac154b9fbb757df0e17b0
Jordan Fite <jordantfite@gmail.com>
2019-01-31 01:22:18 -0600

    Updated the signup page to accept valid signups that match the User model. Model can be edited in the core app. Currently only supports email and password. Detailed user signup will be implemented soon

--- serverapp/core/views.py
+++ serverapp/core/views.py
@@ -4,9 +4,26 @@ from __future__ import unicode_literals
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Signup imports
+from django.contrib.auth import login, authenticate
+from django.shortcuts import render, redirect
+from .forms import SignUpForm
+
 # Create your views here.
 def signup(request):
-    return render(request, 'core/signup.html')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(email=user.email, password=raw_password)
+            login(request, user)
+            # Redirect back to index
+            return redirect('/index')
+    else:
+        form = SignUpForm()
+    return render(request, 'core/signup.html', {'form': form})
+    # return render(request, 'core/signup.html')
 
 def ide(request):
     return render(request, 'core/ide.html')
