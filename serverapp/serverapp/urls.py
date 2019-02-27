fbc7324f02282b0c6ba3c1ab471fcc1f37a608b5
Stannic <Wellwardedx10@gmail.com>
2019-02-26 19:54:00 -0600

    last push to go to master.

--- serverapp/serverapp/urls.py
+++ serverapp/serverapp/urls.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 """serverapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -31,36 +30,3 @@ urlpatterns = [
     url(r'^problems/$', core_views.problems, name='problems'),
     url(r'^scoreboard/$', core_views.scoreboard, name='scoreboard'),
 ]
-=======
-"""serverapp URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
-from django.conf.urls import url
-from django.contrib import admin
-from core import views as core_views
-
-urlpatterns = [
-    # If no path, default to index
-    url(r'^$', core_views.index, name='index'),
-    url(r'^admin/', admin.site.urls),
-    url(r'^signup/$', core_views.signup, name='signup'),
-    url(r'^ide/$', core_views.ide, name='ide'),
-    url(r'^ide-ajax/$', core_views.ideCodeAjax, name='ideCodeAjax'),
-    url(r'^settings/$', core_views.settings, name='settings'),
-    url(r'^index/$', core_views.index, name='index'),
-    url(r'^problems/$', core_views.problems, name='problems'),
-    url(r'^scoreboard/$', core_views.scoreboard, name='scoreboard'),
-]
->>>>>>> 411d5fe5ad1d3d1fb234dbfe2d4b142ebe2e9656
